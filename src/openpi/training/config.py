b35b94fac71d1f08688f6fbb66d2e0d89e97ee01
Arif Ahmed <arif.ahmed105@gmail.com>
2026-04-10 20:05:51 -0700

    Fix checkpoint path in pi05_libero_waypoint_lora config

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -836,7 +836,7 @@ _CONFIGS = [
         ),
         optimizer=_optimizer.AdamW(clip_gradient_norm=1.0),
         weight_loader=weight_loaders.CheckpointWeightLoader(
-            "gs://openpi-assets/checkpoints/pi05_libero"
+            "gs://openpi-assets/checkpoints/pi05_libero/params"
         ),
         freeze_filter=pi0_config.Pi0Config(
             pi05=True,
